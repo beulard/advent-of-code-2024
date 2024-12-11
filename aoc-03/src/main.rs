a7922c56b6130a5058738044112977f06fe5b313
Matthias Dubouchet <beulard@gmail.com>
2024-12-11 23:49:21 +0100

    03p2

--- aoc-03/src/main.rs
+++ aoc-03/src/main.rs
@@ -16,8 +16,34 @@ fn p1() -> i32 {
 }
 
 fn p2() -> i32 {
-    let _input = std::fs::read_to_string("input.txt").unwrap();
-    0
+    let mut input = std::fs::read_to_string("input.txt").unwrap();
+
+    // Remove all text between a "don't()" and the following "do()"
+    let mut cleaned = String::new();
+    while !input.is_empty() {
+        match input.find("don't()") {
+            Some(idx) => {
+                cleaned.push_str(input.drain(..idx + "don't()".len()).as_str());
+                match input.find("do()") {
+                    Some(idx) => input.drain(..idx + "do()".len()),
+                    None => input.drain(..),
+                };
+            }
+            None => cleaned.push_str(input.drain(..).as_str()),
+        }
+    }
+
+    // Same algorithm as p1 on the cleaned input
+    let re = Regex::new(r"mul\((\d+),(\d+)\)").unwrap();
+    let mut score = 0;
+
+    re.captures_iter(&cleaned)
+        .map(|cap| cap.extract())
+        .map(|(_, [left, right])| (left.parse::<i32>().unwrap(), right.parse::<i32>().unwrap()))
+        .for_each(|(left, right)| {
+            score += left * right;
+        });
+    score
 }
 
 fn main() {
