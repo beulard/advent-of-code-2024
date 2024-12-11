daabe25c4833c18429215af068f316d054f1e95f
Matthias Dubouchet <beulard@gmail.com>
2024-12-11 20:13:14 +0100

    01p2

--- aoc-01/src/main.rs
+++ aoc-01/src/main.rs
@@ -1,6 +1,5 @@
 use std::iter::zip;
 fn p1() -> i32 {
-    
     let input = std::fs::read_to_string("input.txt").unwrap();
     let mut vec_left = vec![];
     let mut vec_right = vec![];
@@ -8,7 +7,6 @@ fn p1() -> i32 {
         let (left, right) = line.split_once("   ").unwrap();
         vec_left.push(left.parse::<i32>().unwrap());
         vec_right.push(right.parse::<i32>().unwrap());
-        
     });
     vec_left.sort();
     vec_right.sort();
@@ -20,6 +18,24 @@ fn p1() -> i32 {
     return distance;
 }
 
+fn p2() -> i32 {
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let mut vec_left = vec![];
+    let mut vec_right = vec![];
+    input.lines().for_each(|line| {
+        let (left, right) = line.split_once("   ").unwrap();
+        vec_left.push(left.parse::<i32>().unwrap());
+        vec_right.push(right.parse::<i32>().unwrap());
+    });
+
+    let mut score = 0;
+    for x in vec_left {
+        score += x * vec_right.iter().filter(|&&y| y == x).count() as i32;
+    }
+    return score;
+}
+
 fn main() {
     println!("p1 = {}", p1());
+    println!("p2 = {}", p2());
 }
