d3fd7ea744fd680157ea9d2f6206ed6a099a2df0
Matthias Dubouchet <beulard@gmail.com>
2024-12-14 19:26:17 +0100

    02p2

--- aoc-02/src/main.rs
+++ aoc-02/src/main.rs
@@ -1,3 +1,21 @@
+fn is_safe(report: &[i32]) -> bool {
+    let mut sign = 0;
+    let mut safe = true;
+    for pair in report.windows(2) {
+        let diff = pair[1] - pair[0];
+        if diff.abs() < 1 || diff.abs() > 3 {
+            safe = false;
+            break;
+        }
+        if sign != 0 && diff.signum() != sign {
+            safe = false;
+            break;
+        }
+        sign = diff.signum();
+    }
+    safe
+}
+
 fn p1() -> i32 {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let mut score: i32 = 0;
@@ -8,21 +26,7 @@ fn p1() -> i32 {
             .map(|x| x.parse::<i32>().unwrap())
             .collect::<Vec<_>>();
 
-        let mut sign = 0;
-        let mut safe = true;
-        for pair in values.windows(2) {
-            let diff = pair[1] - pair[0];
-            if diff.abs() < 1 || diff.abs() > 3 {
-                safe = false;
-                break;
-            }
-            if sign != 0 && diff.signum() != sign {
-                safe = false;
-                break;
-            }
-            sign = diff.signum();
-        }
-        if safe {
+        if is_safe(&values) {
             score += 1;
         }
     }
@@ -32,7 +36,36 @@ fn p1() -> i32 {
 
 fn p2() -> i32 {
     let input = std::fs::read_to_string("input.txt").unwrap();
-    0
+    let mut score: i32 = 0;
+
+    for line in input.lines() {
+        let values = line
+            .split_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        // Determine if safe, if not, try again, removing a different number each time
+        let mut safe = false;
+
+        if is_safe(&values) {
+            safe = true;
+        } else {
+            for i in 0..values.len() {
+                let mut report = values.clone();
+                report.remove(i);
+                if is_safe(&report) {
+                    safe = true;
+                    break;
+                }
+            }
+        }
+
+        if safe {
+            score += 1;
+        }
+    }
+
+    return score;
 }
 
 fn main() {
