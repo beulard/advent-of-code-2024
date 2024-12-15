23ed89aa5575072c938f4ccaffc876066e9accce
Matthias Dubouchet <beulard@gmail.com>
2024-12-15 12:31:33 +0100

    05p2

--- aoc-05/src/main.rs
+++ aoc-05/src/main.rs
@@ -1,25 +1,53 @@
 use std::collections::HashMap;
 
+fn is_update_ok(update: &[i32], deps: &HashMap<i32, Vec<i32>>) -> bool {
+    let mut ok = true;
+    let mut updated: Vec<&i32> = Vec::with_capacity(update.len());
+    'update: for page in update {
+        // update not ok if page depends on a page present in the update but not yet updated
+        let depends = match deps.get(&page) {
+            Some(x) => x.iter().filter(|&&p| update.contains(&p)).collect(),
+            None => vec![],
+        };
+
+        for d in &depends {
+            if !updated.contains(d) {
+                ok = false;
+                break 'update;
+            }
+        }
+        updated.push(page);
+    }
+    ok
+}
+
+fn get_dependency_map(rules: &[(i32, i32)]) -> HashMap<i32, Vec<i32>> {
+    let mut deps = HashMap::<i32, Vec<i32>>::new();
+    for (left, right) in rules {
+        match deps.get_mut(&right) {
+            Some(x) => {
+                x.push(*left);
+            }
+            None => {
+                deps.insert(*right, vec![*left]);
+            }
+        }
+    }
+    deps
+}
+
 fn p1() -> i32 {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let (rules, updates) = input.split_once("\n\n").unwrap();
 
     // Construct list of dependencies for each page
-    let mut deps = HashMap::<i32, Vec<i32>>::new();
-    for (left, right) in rules
-        .lines()
-        .map(|line| line.split_once("|").unwrap())
-        .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
-    {
-        match deps.get_mut(&right) {
-            Some(x) => {
-                x.push(left);
-            }
-            None => {
-                deps.insert(right, vec![left]);
-            }
-        }
-    }
+    let deps = get_dependency_map(
+        &rules
+            .lines()
+            .map(|line| line.split_once("|").unwrap())
+            .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
+            .collect::<Vec<(i32, i32)>>(),
+    );
 
     let mut score = 0;
     for update in updates.lines() {
@@ -27,24 +55,8 @@ fn p1() -> i32 {
             .split(',')
             .map(|s| s.parse::<i32>().unwrap())
             .collect::<Vec<_>>();
-        let mut updated: Vec<&i32> = Vec::with_capacity(pages.len());
-        let mut ok = true;
-        'update: for page in &pages {
-            // update not ok if page depends on a page present in the update but not yet updated
-            let depends = match deps.get(&page) {
-                Some(x) => x.iter().filter(|&&p| pages.contains(&p)).collect(),
-                None => vec![],
-            };
 
-            for d in &depends {
-                if !updated.contains(d) {
-                    ok = false;
-                    break 'update;
-                }
-            }
-            updated.push(page);
-        }
-        if ok {
+        if is_update_ok(&pages, &deps) {
             score += pages[pages.len() / 2];
         }
     }
@@ -53,8 +65,58 @@ fn p1() -> i32 {
 }
 
 fn p2() -> i32 {
-    let _input = std::fs::read_to_string("input.txt").unwrap();
-    0
+    let input = std::fs::read_to_string("input.txt").unwrap();
+    let (rules, updates) = input.split_once("\n\n").unwrap();
+
+    // Construct list of dependencies for each page
+    let deps = get_dependency_map(
+        &rules
+            .lines()
+            .map(|line| line.split_once("|").unwrap())
+            .map(|(left, right)| (left.parse().unwrap(), right.parse().unwrap()))
+            .collect::<Vec<(i32, i32)>>(),
+    );
+
+    let mut score = 0;
+    for update in updates.lines() {
+        let pages = update
+            .split(',')
+            .map(|s| s.parse::<i32>().unwrap())
+            .collect::<Vec<_>>();
+
+        let mut ok = true;
+        let mut corrected = pages.clone();
+
+        let mut i = 0;
+        'update: while i < pages.len() {
+            // update not ok if page depends on a page present in the update but not yet updated
+            let depends = match deps.get(&corrected[i]) {
+                Some(x) => x.iter().filter(|&&p| pages.contains(&p)).collect(),
+                None => vec![],
+            };
+
+            for d in &depends {
+                if corrected.iter().position(|x| x == *d).unwrap() > i {
+                    ok = false;
+                    // Invalid update: this page depends on d
+                    // Invert this page and the dependent one to fix
+                    let src_pos = i;
+                    let dest_pos = corrected.iter().position(|x| x == *d).unwrap();
+                    let tmp = corrected[src_pos];
+                    corrected[src_pos] = **d;
+                    corrected[dest_pos] = tmp;
+                    i = src_pos;
+                    continue 'update;
+                }
+            }
+            i += 1;
+        }
+        if !ok {
+            score += corrected[corrected.len() / 2];
+        }
+    }
+
+    score
 }
 
 fn main() {
